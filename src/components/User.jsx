9142129424ab4d23d8b318dbb36684a8b9767e63
Asif Shaik <asif.shaik9032@gmail.com>
2024-02-21 13:12:05 +0530

    Added video section to the User section

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,28 @@
+// VideoPlayer.js
+
 import React from 'react';
-import WIP from './WIP';
+import YouTube from 'react-youtube';
+import '../assets/media.css';
 
-const User = () => {
+const User = ({ videoId }) => {
+  const opts = {
+    height: '90vh',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
-    <WIP/>
-  )
-}
+    <>
+      <h2>YouTube Video Player 2</h2>
+      <div className='video-container'>
+        <YouTube videoId={'PuTe6i8A3Ug'} opts={opts} />
+      </div>
+    </>
 
-export default User
+  );
+};
+
+export default User;
+
